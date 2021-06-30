8f008ae16182a1672b06ba0ec3af611e2ff23a5f
tmzkt21 <e>
2021-06-30 19:05:56 +0900

    store 미완

--- src/main/java/org/bit/store/reply/entity/Reply.java
+++ src/main/java/org/bit/store/reply/entity/Reply.java
@@ -2,6 +2,7 @@ package org.bit.store.reply.entity;
 
 import lombok.*;
 import org.bit.store.board.entity.Board;
+import org.bit.store.common.entity.BaseEntity;
 
 import javax.persistence.*;
 
@@ -11,15 +12,16 @@ import javax.persistence.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
+@ToString(exclude = {"board"})
+// @ManyToOne(fetch = FetchType.LAZY) 레이지사용시 연관관계에있는  exculde  지정한 것에 투스트링을 빼주는것
 @Table(name = "reply")
-public class Reply {
+public class Reply extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long rno;
 
     private String replyText;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Board board;
 }
--- src/test/java/org/bit/store/ReplyRepositoryTests.java
+++ src/test/java/org/bit/store/ReplyRepositoryTests.java
@@ -7,8 +7,13 @@ import org.bit.store.reply.repository.ReplyRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -19,12 +24,30 @@ public class ReplyRepositoryTests {
     ReplyRepository replyRepository;
 
     @Test
-    public void testInsult(){
-        IntStream.rangeClosed(1,5).forEach(todo ->{
-            Board board = Board.builder().bno(1L).build();
-            Reply reply = Reply.builder().replyText("쏘굿"+todo).board(board).build();
+    public void testInsult() {
+        IntStream.rangeClosed(1, 20).forEach(todo -> {
+//            long bno = (int)(Math.random()*200) + 1;
+            Board board = Board.builder().bno(1180L).build();
+            Reply reply = Reply.builder().replyText("쏘굿" + todo).board(board).build();
             replyRepository.save(reply);
         });
+    }
+
+    @Test
+    public void testRead() {
+        Optional<Reply> result = replyRepository.findById(1L);
+        log.info(result);
+        result.ifPresent(todo -> {
+            log.info(todo);
+        });
+    }
+
+    @Test
+    public void testPaging() {
+        Pageable pageable = PageRequest.of(0,10);
+        Page<Reply> result = replyRepository.findAll(pageable);
+        log.info(result);
+        result.getContent().forEach(todo ->log.info(todo));
     }
 
 }
